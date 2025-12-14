8baf16c9aff0ff7e2dbcd7518bcef5d84c30ddea
Piyushh-71 <pc8576917@gmail.com>
2025-12-14 12:56:16 +0530

    added background

---
+++ index.js
@@ -0,0 +1,43 @@
+// // function debounce(fn, delay) {
+// //   let timer;
+// //   return function () {
+// //     clearTimeout(timer);
+// //     timer = setTimeout(fn, delay);
+// //   };
+// // }
+// // document.querySelector("input").addEventListener(
+// //   "input",
+// //   debounce(function () {
+// //     console.log("chalegaa");
+// //   }, 400)
+// // );
+
+// // function debounce(fn, delay) {
+// //   let timer;
+// //   return function () {
+// //     clearTimeout(timer);
+// //     timer = setTimeout(fn, delay);
+// //   };
+// // }
+
+// // document.querySelector("input").addEventListener(
+// //   "input",
+// //   debounce(function () {
+// //     console.log("chalegaa");
+// //   }, 400)
+// // );
+
+// function debounce(fn, delay) {
+//   let timer;
+//   return function () {
+//     clearTimeout(timer);
+//     timer = setTimeout(fn, delay);
+//   };
+// }
+
+// document.querySelector("input").addEventListener(
+//   "input",
+//   debounce(function () {
+//     console.log("chalegaaa");
+//   }, 400)
+// );
